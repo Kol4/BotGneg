41a7d334a1a8b9f15813324904453e21855ed78a
ndubatovka <ndubatovka@belprime.com>
2020-09-23 02:05:26 +0300

    init

--- src/main/java/com/home/server/TelegramBot/Bot.java
+++ src/main/java/com/home/server/TelegramBot/Bot.java
@@ -1,3 +1,5 @@
+package com.home.server.TelegramBot;
+
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
