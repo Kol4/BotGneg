85dad5bf7f702872d0fe1a2ca0183df5bbf10606
ndubatovka <ndubatovka@belprime.com>
2020-09-23 16:59:24 +0300

    add golodni

--- src/main/java/com/home/server/TelegramBot/Bot.java
+++ src/main/java/com/home/server/TelegramBot/Bot.java
@@ -3,18 +3,22 @@ package com.home.server.TelegramBot;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.methods.send.SendSticker;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.security.SecureRandom;
 import java.util.ArrayList;
-import java.util.Random;
+import java.util.List;
+import java.util.regex.Pattern;
 
 public class Bot extends TelegramLongPollingBot {
-    static ArrayList<String> whereWeGoMessages = new ArrayList<>();
-    static ArrayList<String> whereWeGoEatMessages = new ArrayList<>();
-    static ArrayList<String> whereDidWeGoList = new ArrayList<>();
-    static ArrayList<String> whereDidWeGoEatList = new ArrayList<>();
+    private static final Pattern COMPILE = Pattern.compile("[^a-zA-Zа-яА-Я\\s]");
+    static List<String> whereWeGoMessages = new ArrayList<>();
+    static List<String> whereWeGoEatMessages = new ArrayList<>();
+    static List<String> whereDidWeGoList = new ArrayList<>();
+    static List<String> whereDidWeGoEatList = new ArrayList<>();
 
     @Override
     public void onUpdateReceived(Update update) {
@@ -26,20 +30,20 @@ public class Bot extends TelegramLongPollingBot {
                 checkMessageType(inMessage, whereWeGoMessages, whereDidWeGoList);
                 checkMessageType(inMessage, whereWeGoEatMessages, whereDidWeGoEatList);
                 checkPhoto(inMessage);
+                checkGolodni(inMessage);
             }
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
     }
 
-    private void checkMessageType(Message inMessage, ArrayList<String> messages, ArrayList<String> list) throws TelegramApiException {
-        SendMessage outMessage = new SendMessage();
-        String line = inMessage.getText().replaceAll("[^a-zA-Zа-яА-Я\\s]", "").toLowerCase();
+    private void checkMessageType(Message inMessage, List<String> messages, List<String> list) throws TelegramApiException {
+        String line = COMPILE.matcher(inMessage.getText()).replaceAll("").toLowerCase();
         if (messages.contains(line)) {
-            outMessage.setChatId(inMessage.getChatId());
-            Random random = new Random();
-            int index = random.nextInt(list.size());
+            int index = new SecureRandom().nextInt(list.size());
             String item = list.get(index);
+            SendMessage outMessage = new SendMessage();
+            outMessage.setChatId(inMessage.getChatId());
             outMessage.setText(item);
             outMessage.setReplyToMessageId(inMessage.getMessageId());
             execute(outMessage);
@@ -47,14 +51,25 @@ public class Bot extends TelegramLongPollingBot {
     }
 
     private void checkPhoto(Message inMessage) throws TelegramApiException {
-        String line = inMessage.getText().replaceAll("[^a-zA-Zа-яА-Я\\s]", "").toLowerCase();
+        String line = COMPILE.matcher(inMessage.getText()).replaceAll("").toLowerCase();
         if (line.equalsIgnoreCase("детка ты словно")) {
-            SendPhoto sendPhoto = new SendPhoto().setChatId(inMessage.getChatId());
+            SendPhoto sendPhoto = new SendPhoto();
+            sendPhoto.setChatId(inMessage.getChatId());
             sendPhoto.setPhoto("https://sun2.velcom-by-minsk.userapi.com/Z3KMd8BMdOI1krfrsULO52r60YSM8N_jCoEI-g/MyxvRnf_FTc.jpg");
             execute(sendPhoto);
-            SendMessage sendMessage = new SendMessage().setChatId(inMessage.getChatId());
-            sendMessage.setText("Cхожу с ума от твоих подлокотников");
-            execute(sendMessage);
+            SendMessage outMessage = new SendMessage().setChatId(inMessage.getChatId());
+            outMessage.setText("Cхожу с ума от твоих подлокотников");
+            execute(outMessage);
+        }
+    }
+
+    private void checkGolodni(Message inMessage) throws TelegramApiException {
+        String line = COMPILE.matcher(inMessage.getText()).replaceAll("").toLowerCase();
+        if (line.equalsIgnoreCase("хочу кушоть")) {
+            SendSticker sticker = new SendSticker();
+            sticker.setChatId(inMessage.getChatId());
+            sticker.setSticker("CAACAgIAAxkBAAEBXMRfa1MNpNIGYa1mdTcUx7bD-imMgQACuAADmY-lB4KvrWz3nRM1GwQ");
+            execute(sticker);
         }
     }
 
--- src/main/java/com/home/server/TelegramBot/Main.java
+++ src/main/java/com/home/server/TelegramBot/Main.java
@@ -7,7 +7,6 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 public class Main {
     public static void main(String[] args) throws TelegramApiRequestException {
         ApiContextInitializer.init();
-
         TelegramBotsApi botsApi = new TelegramBotsApi();
         botsApi.registerBot(new Bot());
     }
